81759b441a6043ea56a1c00a12a48242f3f890ab
Sangini Sangle <122911139+SanginiSangle22@users.noreply.github.com>
2023-06-29 00:59:20 +0530

    Add files via upload

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-@+$8yg3-*n!o_neew)xd+a4c9z5ab3e3b*q01876uq+d_!)^_*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','localhost','127.0.0.1']
 
 
 # Application definition
